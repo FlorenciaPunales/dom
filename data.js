91854ae5e3246df22d8a549c74cb3da18de71477
florenciapunales <fpp2027@gmail.com>
2023-01-31 12:47:46 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,57 @@
+const data = [
+	{
+		"name": "Ian Hines",
+		"phone": "1-244-252-7244",
+		"email": "id.nunc@outlook.ca",
+		"country": "Nigeria"
+	},
+	{
+		"name": "Orli Goodwin",
+		"phone": "(268) 550-8177",
+		"email": "euismod.mauris@aol.org",
+		"country": "Spain"
+	},
+	{
+		"name": "Pearl Allen",
+		"phone": "(603) 742-6772",
+		"email": "a@hotmail.edu",
+		"country": "Vietnam"
+	},
+	{
+		"name": "Igor Dotson",
+		"phone": "(550) 840-0867",
+		"email": "vulputate.risus@hotmail.com",
+		"country": "India"
+	},
+	{
+		"name": "Zachery Haynes",
+		"phone": "(557) 612-3755",
+		"email": "a.dui@google.com",
+		"country": "South Korea"
+	},
+	{
+		"name": "Mario ",
+		"phone": "1-244-252-7244",
+		"email": "id.nunc@outlook.ca",
+		"country": "Nigeria"
+	},
+	{
+		"name": "Romi",
+		"phone": "(268) 550-8177",
+		"email": "euismod.mauris@aol.org",
+		"country": "Spain"
+	},
+	{
+		"name": "Lucre",
+		"phone": "(603) 742-6772",
+		"email": "a@hotmail.edu",
+		"country": "Vietnam"
+	},
+	{
+		"name": "Kevin",
+		"phone": "(550) 840-0867",
+		"email": "vulputate.risus@hotmail.com",
+		"country": "India"
+	},
+	
+]
---
+++ main.js
@@ -0,0 +1,91 @@
+// let titulo = document.getElementById("titulo")
+
+// titulo.classList.add("color")
+// console.log(titulo.classList)
+
+// Metodos para traernos los nodos del árbol.
+
+// console.log(document.getElementById("titulo")) 
+
+// console.log(document.getElementsByClassName("soy-titulo"))
+
+// console.log(document.getElementsByTagName("a"))
+
+// console.log(document.querySelector("div"))
+
+// console.log(document.querySelectorAll("div"))
+
+// console.log(document.createElement("div"))
+// console.log(document.createElement("h1"))
+
+
+// let titulo = document.getElementById("titulo")
+
+// titulo.className += "segunda-clase"
+
+// console.log(titulo.classList)
+
+// const main = document.querySelector("main")
+// const div = document.createElement("div")
+
+
+// div.className = "div-principal"
+// div.textContent = "Hola, soy un Div"
+// console.log(div)
+
+// main.appendChild(div)
+
+// let listaContenedor = document.getElementById("lista")
+// let frutas = ["banana", "manzana", "pera", "papaya"]
+
+// // Primer paso: Crear Li
+
+// // Segundo paso: Por cada LI se muestre texto
+// for (let fruta of frutas) {
+//     let listaFrutas = document.createElement("li")
+//     listaFrutas.textContent = fruta
+//     console.log(listaFrutas)
+//     listaContenedor.appendChild(listaFrutas)
+    
+// }
+
+// let personas = data
+// let lista = document.getElementById("nombres") 
+// console.log(data[0].name)
+
+// // Primer paso crear los li
+// for (let persona of personas) {
+//     let listaPersona = document.createElement("li")
+//     listaPersona.textContent = persona.name
+//     console.log(listaPersona)
+//     lista.appendChild(listaPersona)
+// }
+
+//  let lista = document.getElementById("lista") 
+//  let frutas = ["banana", "manzana", "pera", "papaya"]
+
+//  for (let fruta of frutas) {
+//     lista.innerHTML += `<li class="lista text-white">${fruta}</li>`
+//  }
+
+// const main= document.querySelector("main")
+// let personasInfo = data
+
+// for (let personaInfo of personasInfo) {
+//     let divContenedor = document.createElement("div")
+//     divContenedor.className = "carta-info"
+//     divContenedor.style.width = "18rem"
+//     divContenedor.innerHTML = `<div>
+//     <h5>${personaInfo.name}</h5>
+//     <p>${personaInfo.country}</p>
+//     <a href="mailto:${personaInfo.email}">${personaInfo.email}</a>
+//     </div>`
+//     main.appendChild(divContenedor)
+    
+
+// }
+
+
+
+
+
